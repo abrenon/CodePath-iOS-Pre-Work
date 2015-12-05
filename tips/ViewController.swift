53f8e4801f068fcadec4a2f7d247aedddc06e8ec
Allie Rose Brenon <angelrosie10@aol.com>
2015-12-05 13:03:13 -0500

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Alexandria Brenon on 12/5/15.
+//  Copyright © 2015 Alexandria Brenon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
