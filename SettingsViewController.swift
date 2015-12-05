fbc71aad3dac3034e280eb50f60d73f4e7616b5e
abrenon <allie.rose.brenon@gmail.com>
2015-12-05 14:58:10 -0500

    Created a tip calculator

---
+++ SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Alexandria Brenon on 12/5/15.
+//  Copyright © 2015 Alexandria Brenon. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeDefault(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(defaultControl.selectedSegmentIndex, forKey: "default_tip")
+        defaults.synchronize()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let def = defaults.integerForKey("default_tip")
+        defaultControl.selectedSegmentIndex = def
+    }
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func billEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipPercent = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = billField.text!._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercent
+        var total = billAmount+tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let def = defaults.integerForKey("default_tip")
+        tipControl.selectedSegmentIndex = def
+    }
 
+    
 }
 
